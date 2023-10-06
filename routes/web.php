7f547484eef31ded58fab70035573ab64ee7362f
ShutaTanaka <shuta.tanaka@eeeeg.tokyo>
2023-10-06 18:04:55 +0900

    バリデーションについて学習開始

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+//お問合せフォーム
+Route::get('/contact', [ContactController::class,'index'])->name('contact');
+Route::post('/contact', [ContactController::class,'sendMail']);
+Route::get('/contact/complate', [ContactController::class,'complete'])->name('contact.complete');
